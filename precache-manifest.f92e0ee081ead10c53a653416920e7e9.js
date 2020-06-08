02fb4b84ce7e9aec9890c4313814a7e7b5a14dcc
mingjing tang <tmjcsh@gmail.com>
2020-06-08 12:20:06 -0400

    Updates

---
+++ precache-manifest.f92e0ee081ead10c53a653416920e7e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2c8a34a4d3bdd9b92798e654d4dc2f8",
+    "url": "/book-club/index.html"
+  },
+  {
+    "revision": "18bf9403f6c81f9983e1",
+    "url": "/book-club/static/css/main.5828d8c5.chunk.css"
+  },
+  {
+    "revision": "b00ce3708b4151b9a3c8",
+    "url": "/book-club/static/js/2.74a1e102.chunk.js"
+  },
+  {
+    "revision": "88a2ddaac1b39a1133c0546de0fcb896",
+    "url": "/book-club/static/js/2.74a1e102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18bf9403f6c81f9983e1",
+    "url": "/book-club/static/js/main.0ed1013e.chunk.js"
+  },
+  {
+    "revision": "72b9f1d7f0435216ad5e",
+    "url": "/book-club/static/js/runtime-main.77b9187e.js"
+  }
+]);
